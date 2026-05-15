58609bef37c0362be42192f5f1fdb924685f9223
GOgden2123 <101489409+GOgden2123@users.noreply.github.com>
2026-05-14 20:37:31 -0400

    Updated Snake Game Flow

--- Snake.py
+++ Snake.py
@@ -9,7 +9,11 @@ up = 2
 left = 3
 gameOverFont = pg.font.Font(None, 36)
 highScoreFont = pg.font.Font(None, 36)
+livesFont = pg.font.Font(None, 36)
 gamePaused = False
+mainMenuState = 1
+playingState = 2
+scoringState = 3 
 
 #Creating a Snake
 class Snake (RGBSurface):
@@ -33,6 +37,7 @@ class Snake (RGBSurface):
         self.fruitEaten = pg.mixer.Sound(file='sfx/eatingFruit.wav')
         self.soundPlaying = False 
         return
+    #end __init__
 
     def respawn (self):
         self.speed = 20
@@ -41,10 +46,13 @@ class Snake (RGBSurface):
         self.rect.topleft = (0,0)
         for i in self.bodyPosition[1:]:
             i.x = -100
+        return
 
-        #self.lives = self.lives -1
+    def restartBody(self):
+        self.bodyParts = [self.img]
+        self.bodyPosition = [self.rect,pg.Rect(-20,0,20,20)]
 
-        return 
+        return
 
     def update (self, deltaTime):
         self.moveTimePast += deltaTime
@@ -81,6 +89,7 @@ class Fruit (RGBSurface):
         #Fruit Image/Creation 
         self.img.fill((238,75,43))
         return
+    #emd __init__
 
     def FruitRespawn (self, screen_size):
         random_x = 20 * randint(0, int(800/20)-20)
@@ -94,9 +103,11 @@ class SnakeState (GameState):
     def __init__(self) -> None:
         super().__init__()
         
-        #self.bodyParts: list[Snake] = list([Snake()])
         self.Fruit = Fruit()
         self.Snake = Snake()
+        self.highScoreFont = pg.font.Font(None, 36)
+        self.playerScores = 0
+
         return
     #end __init__
 
@@ -121,14 +132,11 @@ class SnakeState (GameState):
         #Setting Snake Growth
         if self.Snake.rect.colliderect(self.Fruit.rect):
             self.Fruit.FruitRespawn(self.renderSize)
-            #self.Snake.bodyPosition.append(self.Snake.bodyPosition[-1].move(0,0))
-            new_segment = self.Snake.bodyPosition[-1].copy()
-            self.Snake.bodyPosition.append(new_segment)
-            self.Snake.highScore += 10
-            #if self.Snake.soundPlaying == False:
-                #self.Snake.soundPlaying = True
+            newSegment = self.Snake.bodyPosition[-1].copy()
+            self.Snake.bodyPosition.append(newSegment)
+            self.playerScores += 10
             self.Snake.fruitEaten.play()
-                 #self.Snake.fruitEaten.stop()
+            
             #Increasing Snake Speed when fruit is eaten
             if self.Snake.moveTimer > 40:
                 self.Snake.moveTimer -= 2
@@ -154,28 +162,38 @@ class SnakeState (GameState):
         #Win/Lose Conditions and Game Font
         if self.Snake.lives <= 0:
             global gamePaused
-            self.Snake.highScore = 0
             gamePaused = True
             if self.keysDown[pg.K_SPACE]:
                 gamePaused = False
+                self.playerScores = 0
                 self.Snake.lives = 4
                 self.Snake.respawn()
+                self.Snake.restartBody()
                 self.Fruit.FruitRespawn(self.renderSize)
+                self.exitCode = playingState
 
         return
+    #end update 
 
     def render (self):
         self.renderBuffer.fill((0,0,0))
         self.Snake.render(self.renderBuffer)
         self.Fruit.render(self.renderBuffer)
-        score_surf = highScoreFont.render(f"Score: {self.Snake.highScore}", True, (100, 255, 255))
-        self.renderBuffer.blit(score_surf, (675, 10))
+        scoreSurf = highScoreFont.render(f"Score: {self.playerScores}", True, (100, 255, 255))
+        livesSurf = livesFont.render(f"Lives: {self.Snake.lives}", True, (100, 255, 255))
+        self.renderBuffer.blit(scoreSurf, (675, 10))
+        self.renderBuffer.blit(livesSurf, (675, 35))
         if gamePaused:
+            self.exitCode = scoringState
+            self.highScoreText = self.highScoreFont.render("Current High Score: %d" %(self.playerScores), True, (255, 255, 255))
+            self.renderBuffer.blit(highScoreFont.render("Current High Score: %d" %(self.playerScores), True, (255,255,255)), ((100,100), (200,300)))
             self.renderBuffer.fill((22,4,75))
-            self.renderBuffer.blit(gameOverFont.render("Game Over!" , False, (255,255,255)), ((100,100), (200,300)))
             self.renderBuffer.blit(gameOverFont.render("Press SPACE to play again." , False, (255,255,255)), ((100,200), (200,300)))
-        #pg.display.flip()
-        return 
+            self.renderBuffer.blit(self.highScoreText, (285,300))
+            pg.display.flip()
+            
+        return
+    #end render
 #end SnakeState
 
 class MenuState (GameState):
@@ -183,6 +201,7 @@ class MenuState (GameState):
         super().__init__()
         self.titleFont = pg.font.Font(None, 70)
         self.startFont = pg.font.Font(None, 40)
+        self.scoreFont = pg.font.Font(None, 40)
 
         return
         #end __init__
@@ -190,7 +209,7 @@ class MenuState (GameState):
     def update (self):
         if self.keysDown[pg.K_RETURN]:
             self.transitionState = SnakeState()
-            self.exitCode = 1
+            self.exitCode = mainMenuState 
 
         return 
         #end update
@@ -199,42 +218,112 @@ class MenuState (GameState):
         self.renderBuffer.fill((20,20,50))
         self.titleText = self.titleFont.render("SNAKE", True, (255,255,255))
         self.playText = self.startFont.render("Press ENTER to play", True, (255,255,255))
-        self.renderBuffer.blit(self.titleText, (250,250))
-        #self.renderBuffer.blit(self.titleFont, (230, 200))
-        self.renderBuffer.blit(self.playText, (270, 350))
-
+        self.renderBuffer.blit(self.titleText, (285,300))
+        self.renderBuffer.blit(self.playText, (250, 400))
         pg.display.flip()
 
         return
         #end render
-
 #end MenuState
 
-class SnakeGame (Game):
+class highScoreState (GameState):
+    def __init__ (self) -> None:
+        super().__init__(pg.surface.Surface((20,20)))
+        self.highScoreFont = pg.font.Font (None, 40)
+        self.playerScores = 0
+        self.previousScore = 0
+        self.getHighScores()
+                                         
+        return
+    #end __init__
 
+    def getHighScores (self):
+        with open('data/SnakeHighScore.txt', 'r') as highScores:
+              self.playerScores = int(highScores.read().strip())
+
+        return
+    #end getHighScores
+
+    def setNewScore (self, newScore):
+        self.playerScores = newScore
+        if self.playerScores > self.previousScore:
+            self.previousScore = self.playerScores
+            with open('data/SnakeHighScore.txt', 'w') as highScores:
+                highScores.write(str(self.playerScores))
+            
+    def update (self):
+        super().update()
+        if self.keysDown[pg.K_SPACE]:
+            self.exitCode = scoringState
+            
+        return
+    #end update
+
+    def showHighScores ():
+        with open('data/SnakeHighScore.txt', 'r') as highScores:
+            return int(highScores.read())
+        with open("data/SnakeHighScore.txt", "w") as highScores:
+            highScores.write(str(player1Score))
+            highScores.write(str(player2Score))
+
+        return
+    #end showHighScores
+
+    def render (self):
+        super().render()
+        self.highScoreText = self.highScoreFont.render("Current High Score: %d" %(self.playerScores), True, (255, 255, 255))
+        self.renderBuffer.blit(self.highScoreText, (285,300))
+        pg.display.flip()
+
+        return
+    #end render
+#end highScoreState 
+
+class SnakeGame (Game):
     def __init__ (self) -> None:
         super().__init__(name = 'Snake', displaySize = [800,600])
         self.menuState = MenuState()
         self.playState = SnakeState()
         self.switchState (self.menuState)
+        self.highScoreState = highScoreState()
         
 
         return
+    #end __init__
 
     def update (self):
         super().update()
-        if self._state.exitCode == 1:
+        if self._state.exitCode == mainMenuState:
             self.switchState(self.playState)
+            self._state.exitCode = 0
+        elif self._state.exitCode == playingState:
+            if isinstance(self._state, SnakeState):
+                #Getting the new score from the play session
+                score = self._state.Snake.highScore
+                #Going from the high score screen to playing again
+                self.highScoreState.setNewScore(score)
+                self.switchState(self.playState)
+
+        
+        #elif self._state.exitCode == scoringState:
+            #self.switchState(self.menuState)
+            #self._state.exitCode = 0
+            #self.highScoreState.setNewScore(score)
+            #self.switchState(self.highScoreState)
+    
+              
 
         return
+    #end update 
 
     def render (self):
         super().render()
 
-        return 
+        return
+    #end render
 
 
-    #end SnakeGame
+#end SnakeGame
 
 def main() -> None:
     myGame: Game = SnakeGame()
